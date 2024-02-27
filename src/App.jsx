fd2bb64b6314d0ed5d716e088fbf9861e1dcd16e
ViktoriiaPavliukh <victoriataran.mail@gmail.com>
2024-02-27 12:54:25 +0000

    add routes to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import { useState } from 'react'
-import './App.css'
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./views/Home";
+import About from "./views/About";
+// import { Home, About } from "./views/index";
+import "./App.css";
 
 function App() {
-
   return (
-    <h1>Hello world!</h1>
-  )
+    <BrowserRouter>
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+    </nav>  
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
