8ab0a03a24b0f98847dfec733ec2496402b00310
ViktoriiaPavliukh <victoriataran.mail@gmail.com>
2024-02-28 13:39:04 +0000

    create nested routes and  HostLayout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./views/Home";
 import About from "./views/About";
 import Vans from "./views/Vans";
@@ -7,25 +7,27 @@ import Vans from "./views/Vans";
 import "./App.css";
 import "./server";
 import VanDetail from "./views/VanDetail";
+import Layout from "./compoments/Layout";
+import Dashboard from "./views/Host/Dashboard";
+import Income from "./views/Host/Income";
+import Reviews from "./views/Host/Reviews";
+import HostLayout from "./compoments/HostLayout";
 
 function App() {
   return (
     <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">
-          #VANLIFE
-        </Link>
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
-        </nav>
-      </header>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path="/vans/:id" element={<VanDetail />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<VanDetail />} />
+          <Route path="/host" element={<HostLayout />}>
+            <Route path="/host" element={<Dashboard />} />
+            <Route path="/host/income" element={<Income />} />
+            <Route path="/host/reviews" element={<Reviews />} />
+          </Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   );
