d1206ff04f1d63cc06688a53de3ac322686e32a3
ViktoriiaPavliukh <victoriataran.mail@gmail.com>
2024-02-28 10:55:46 +0000

    add Vans page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./views/Home";
 import About from "./views/About";
+import Vans from "./views/Vans";
 // import { Home, About } from "./views/index";
 import "./App.css";
 import "./server";
@@ -16,11 +17,13 @@ function App() {
         <nav>
           <Link to="/">Home</Link>
           <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
       </Routes>
     </BrowserRouter>
   );
