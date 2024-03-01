ec1de76886d890cd43bf6c1a707423ac3ce3e827
ViktoriiaPavliukh <victoriataran.mail@gmail.com>
2024-03-01 20:23:37 +0000

    add buttons to filter vans

--- src/views/Vans.jsx
+++ src/views/Vans.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 
 export default function Vans() {
+  const [searchParams, setSearchParams] = useSearchParams();
   const [vans, setVans] = useState([]);
+
+  const typeFilter = searchParams.get("type");
 
   useEffect(() => {
     fetch("/api/vans")
       .then((res) => res.json())
       .then((data) => setVans(data.vans));
   }, []);
+
+  const displayedVans = typeFilter
+    ? vans.filter((van) => van.type === typeFilter)
+    : vans;
 
   const vanElements = vans.map((van) => (
     <div key={van.id} className="van-tile">
@@ -34,6 +41,32 @@ export default function Vans() {
   return (
     <div className="van-list-container">
       <h1>Explore our van options</h1>
+      <div className="van-list-filter-buttons">
+        <button
+          onClick={() => setSearchParams("?type=simple")}
+          className="van-type simple"
+        >
+          Simple
+        </button>
+        <button
+          onClick={() => setSearchParams("?type=luxury")}
+          className="van-type luxury"
+        >
+          Luxury
+        </button>
+        <button
+          onClick={() => setSearchParams("?type=rugged")}
+          className="van-type rugged"
+        >
+          Rugged
+        </button>
+        <button
+          onClick={() => setSearchParams("")}
+          className="van-type clear-filters"
+        >
+          Clear filter
+        </button>
+      </div>
       <div className="van-list">{vanElements}</div>
     </div>
   );
